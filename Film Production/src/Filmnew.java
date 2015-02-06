d816aeddbbec77c5ac770e44d9ec2614cd2597b9
Yuvprime <yuvztikode@gmail.com>
2015-02-06 12:35:27 +0530

    Added new xml file. Solved using depth.

--- Film Production/src/Filmnew.java
+++ Film Production/src/Filmnew.java
@@ -9,7 +9,8 @@ import org.xmlpull.v1.XmlPullParserFactory;
 public class Filmnew {
 
 	static String text;
-   
+   static  String prevtag;
+
     static int c=0;
 	
 	public static void main(String[] args) throws XmlPullParserException, IOException
@@ -20,24 +21,39 @@ public class Filmnew {
 		 factory.setNamespaceAware(true);
          XmlPullParser xpp = factory.newPullParser();
          
-         File xmlFile = new File("employees.xml");
+         File xmlFile = new File("emp.xml");
          FileInputStream fis = new FileInputStream(xmlFile);
          xpp.setInput(fis, null);
         // xpp.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
          int eventType=xpp.getEventType();
          xpp.nextTag();
-         boolean correctSection = false;
          while (eventType!= XmlPullParser.END_DOCUMENT) {
              String name = xpp.getName();
-           
-
-       		 
+             
+             if(name!=null)
+             {
+            	 //System.out.println(xpp.getName() + " : " +xpp.getDepth());
+             }
+            	
         		 switch (eventType) {
                  case XmlPullParser.START_TAG:
                      if (name.equalsIgnoreCase("item")) {
                     	 System.out.println("This is item start tag");                	
-                    	
-                     }	 
+  	 
+                     }	
+                     
+                     if (name.equalsIgnoreCase("id")) {
+                    	 if (xpp.getDepth()<3) {
+                    		 
+                    		 eventType = xpp.next();
+                    		 continue;
+							
+						}
+                    	 else
+                    	 {
+                    	 System.out.println("This is id start tag");                	
+                    	 }
+                     }
       
                      break;
   
@@ -48,15 +64,29 @@ public class Filmnew {
                  case XmlPullParser.END_TAG:
                 	 if (name.equalsIgnoreCase("item")) {
                     	 System.out.println("This is item end tag");
-                     }                	
+                     } 
+                	 if (name.equalsIgnoreCase("id")) {
+                		 
+                		 	if (xpp.getDepth()<3) {
+                           	 System.out.println("This is id end tag depth less than 3 : " +text);
+
+                    		 eventType = xpp.next();
+                    		 continue;
+							
+						}
+                		 	
+                    	 System.out.println("This is id end tag : " +text);
+                     }   
                 	 break;
                 	 
                  default:
                      break;
                
-      
-         }
-         
+                     
+                     
+        		 		} // end of switch
+            	  
+              
         		 eventType = xpp.next();
          }
          
